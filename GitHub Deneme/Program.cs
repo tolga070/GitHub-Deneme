8a7b2e1d240fcb283f15b9333162e0504516c8c6
Tolga <tolgabakirtas@gmail.com>
2018-07-01 22:13:15 +0300

    2

--- GitHub Deneme/Program.cs
+++ GitHub Deneme/Program.cs
@@ -9,22 +9,29 @@ namespace GitHub_Deneme
     class Program
       
     {
-        
+        public static Random timer = new Random();
 
         static void Main(string[] args)
         {
-
-           Console.SetWindowSize(60, 35);
            
-            Console.WriteLine("> ");
-            Console.SetCursorPosition(16, 9);
+            int bekleme = timer.Next(1000, 7000);
+            
 
+            Console.SetWindowSize(100, 30);
+             Console.SetCursorPosition(16, 9);
+        Console.WriteLine("> Acquiring signal... ");
+            
+            System.Threading.Thread.Sleep(bekleme);
+
+            Console.WriteLine("> Signal acquired press any key to continue...");
+            Console.ReadKey();
+
+           
             Console.WriteLine("Hello! Can you hear me?");
             Console.WriteLine("1 - Yes I can hear you || 2 - Who is this? ");
-            int answer1 = int.Parse(Console.ReadLine());
 
-            
-            switch (answer1)
+        int answer = int.Parse(Console.ReadLine());
+            switch (answer)
             {
                 case 1:
                     Console.WriteLine("Oh! Thank god finally I have been trying to reach out to somebody for hours. Call me P.H.");
@@ -39,6 +46,10 @@ namespace GitHub_Deneme
 
             Console.WriteLine("1 - Nice to meet you P.H. || 2 - Why did you call me? ");
             Console.ReadLine();
+
+            Console.WriteLine("So I kinda need your help. I'm part of a research group and we are working on a project in the middle of nowhere");
+            
+          
 
 
             //int luck = CanDEV.Zar();
