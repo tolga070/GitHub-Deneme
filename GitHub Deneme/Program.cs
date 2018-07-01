ce6effae477f794d286109fc8b70db723a3e91ad
Tolga <tolgabakirtas@gmail.com>
2018-07-01 23:24:01 +0300

    yine aynı şey

--- GitHub Deneme/Program.cs
+++ GitHub Deneme/Program.cs
@@ -49,8 +49,8 @@ namespace GitHub_Deneme
             Console.ReadLine();
 
             Console.WriteLine(@"So I kinda need your help. I'm part of a research group and we are working on a project in the 
-middle of nowhere. Problem is emergency alarms are going crazy so I'm suppose to leave the building but the damn
-door is stuck.");
+middle of nowhere. Problem is emergency alarms are going crazy so I'm suppose to leave the building
+but the damndoor is stuck.");
             Console.WriteLine("1 - Well what do you have around you? || 2 - What do you want me to do? || 3 - Define nowhere. ");
            int answer2 = int.Parse(Console.ReadLine());
             switch (answer2)
